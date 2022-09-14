75a800250b5e911fdc99871181eb3b5938ec2398
Dee Pabst <dee@canva.com>
2022-09-14 11:07:51 +1000

    Added User model updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,37 @@
-
 Airplane.destroy_all
 
 Airplane.create!(
 
-    name: '747',
-
-    column: 4,
-
-    row: 24
-
+  name: "747",
+  column: 4,
+  row: 24,
 
 )
 
-puts "Created #{ Airplane.count } airplane."
+puts "Created #{Airplane.count} airplane."
 
-
-# rails g model Flight date:date flight:string origin:string destination:string 
+# rails g model Flight date:date flight:string origin:string destination:string
 
 Flight.destroy_all
 
 Flight.create!(
 
-    date: Date.parse('December 09, 2011'),
-    flight: '13',
-    origin: 'SYD',
-    destination: 'SUV'
+  date: Date.parse("December 09, 2011"),
+  flight: "13",
+  origin: "SYD",
+  destination: "SUV",
 
 )
 
-puts "Created #{ Flight.count } flight."
+puts "Created #{Flight.count} flight."
 
-# rails 
+# rails
+
+#################### Create Users ######################
+User.destroy_all
+
+User.create! name: "Dee"
+User.create! name: "Craig"
+User.create! name: "Wen"
+
+puts "Created #{User.count} users."
