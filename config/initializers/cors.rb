29771654876e2d5f0a4becdc9c17d2c500478ec3
Dee Pabst <dee@canva.com>
2022-09-14 18:02:00 +1000

    flight index now returns JSON and the CORS issue resolved

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,7 @@
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  
-#     allow do
-#       origins '*'  # set CORS header
-#       resource '*', headers: :any, methods: [:get, :post, :patch, :put] 
-#       # handle OPTIONS requests from axios.post() "pre-flight" check
-#     end
-  
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "*"  # set CORS header
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put]
+    # handle OPTIONS requests from axios.post() "pre-flight" check
+  end
+end
