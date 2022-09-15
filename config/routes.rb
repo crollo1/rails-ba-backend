2b456034b1e0c645c17ddc2a1d64e9a706fbeb6f
Dee Pabst <dee@canva.com>
2022-09-15 14:20:00 +1000

    fix flights/:id routing to wrong action (show) problem by prefixing /api/ . Added include to the render json to include associations of flights

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
-    
-  
+
   # API endpoints for Secret model
   # - to be used by React frontend AJAX requests
 
-  get '/airplanes' => 'airplanes#index'       # Airplane.all
-  post '/airplanes' => 'airplanes#create'     # Airplane.create
-  get '/flights/:origin/:destination' => 'flights#show' # Flight search results
-  
-  
+  get "/airplanes" => "airplanes#index"       # Airplane.all
+  post "/airplanes" => "airplanes#create"     # Airplane.create
+  get "/flights/:origin/:destination" => "flights#show" # Flight search results
+
   resources :airplanes
   resources :flights
   resources :user
   resources :reservations
-  
-  get '/flights/:id' => 'flights#select_seat' # Flight 
 
+  get "/api/flights/:id" => "flights#select_seat" # Flight
 end
